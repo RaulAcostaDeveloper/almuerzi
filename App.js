1097eaeec21603fdac6b1633e8556992d06ad985
Raúl <raulacostadeveloper@outlook.com>
2022-05-24 14:06:20 -0500

    almuerzi

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Almuerzi</Text>
       <StatusBar style="auto" />
     </View>
   );
