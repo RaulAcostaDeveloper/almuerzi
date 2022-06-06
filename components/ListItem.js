7aabe8504c82a17b2409d8c1a7ece4989ee3cbca
Raúl <raulacostadeveloper@outlook.com>
2022-06-06 13:51:41 -0500

    fonts Personalizadas

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default ({name, onPress}) => {
     console.log(name);
+    const [loaded] = useFonts({
+        Kdam_Thmor_Pro: require('../assets/fonts/Kdam_Thmor_Pro/KdamThmorPro-Regular.ttf'),
+    });
+      
+    if (!loaded) {
+    return null;
+    }
     return (
         <TouchableOpacity onPress={onPress} style= {styles.container}>
             <Text style= {styles.text}>{name}</Text>
@@ -20,5 +28,6 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize:18,
+        fontFamily:'Kdam_Thmor_Pro',
     },
 })
